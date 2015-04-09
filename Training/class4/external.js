a20013049f9218c78ad7bfca241b49dc6b8432d0
shahidul islam <shahidulmail07@gmail.com>
2015-04-08 22:41:01 -0400

    add folder

---
+++ Training/class4/external.js
@@ -0,0 +1,16 @@
+
+		function showPassword()
+		{
+			var a = document.getElementById("p").value;
+			
+			var b = document.getElementById("cp").value;
+			
+			if (a!=b)
+			{
+			alert("password not match");
+
+			}
+		}
+				
+		
+	
